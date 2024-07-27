3252702bcb262079a6e9d26287e764af2d2b428e
YourAngel64 <angelyadiel8776@gmail.com>
2024-07-27 14:55:06 -0500

    Refactorized code and added simple urls to index.js

--- homura_app/src/index.js
+++ homura_app/src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "./style.css"
 import App from './App';
 import UserSign from "./User_Sign"
+import Home from './Home';
 import Prueba from "./Prueba"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UserSign />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/login' element={<App />}></Route>
+        <Route path='/sign-in' element={<UserSign />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
